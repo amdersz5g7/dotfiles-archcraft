f18dc156c7cb5321543b44672df704cc20834163
V8-ARCH <amdersz5g7@gmail.com>
2022-07-21 06:46:02 +0700

    Initial commit

---
+++ dot_config/openbox/polybar/default/executable_batre_bt.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# upower -i `upower -e | grep '/org/freedesktop/UPower/devices/headset_dev_18_95_52_B1_B9_95'` | grep -E "percentage"
+
+upower -i `upower -e | grep '/org/freedesktop/UPower/devices/headset_dev_18_95_52_B1_B9_95'` | grep -E percentage|xargs|cut -d' ' -f2|sed s/%//
