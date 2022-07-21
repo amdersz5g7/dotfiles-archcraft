071789930ddb9f992aa3f6a1e900e4508d3b2515
V8-ARCH <amdersz5g7@gmail.com>
2022-07-21 08:28:11 +0700

    update content

--- dot_config/openbox/polybar/default/executable_batre_bt.sh
+++ dot_config/openbox/polybar/default/executable_batre_bt.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
+# reference: https://askubuntu.com/questions/69556/how-do-i-check-the-batterys-status-via-the-terminal
+
 # upower -i `upower -e | grep '/org/freedesktop/UPower/devices/headset_dev_18_95_52_B1_B9_95'` | grep -E "percentage"
-
-# https://askubuntu.com/questions/69556/how-do-i-check-the-batterys-status-via-the-terminal
-
 upower -i `upower -e | grep '/org/freedesktop/UPower/devices/headset_dev_18_95_52_B1_B9_95'` | grep -E percentage|xargs|cut -d' ' -f2|sed s/%//
